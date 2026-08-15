38bd5675d86c7f2fc057e354692b0a22d8c4f243
ramagbo <amagboroland@yahoo.fr>
2026-08-15 14:25:28 -0400

    error handling

--- server.py
+++ server.py
@@ -1,7 +1,9 @@
-from flask import Flask, render_template, request 
+"""Flask-based Emotion Detector web application."""
+from flask import Flask, render_template, request
+
 from EmotionDetection.emotion_detection import emotion_detector
 
-#Initiate the flask app 
+#Initiate the flask app
 app = Flask("Emotion Detector")
 
 @app.route("/emotionDetector")
@@ -13,20 +15,27 @@ def sent_analyzer():
     '''
     text_to_analyze = request.args.get('textToAnalyze')
 
-    # Pass the text to the sentiment_analyzer function and store the response 
+    # Pass the text to the sentiment_analyzer function and store the response
     response = emotion_detector(text_to_analyze)
 
-    # Extract the label and score from the response 
-    anger = response['anger'] 
+    # Extract the label and score from the response
+    anger = response['anger']
     disgust = response['disgust']
     fear = response['fear']
     joy = response['joy']
     sadness = response['sadness']
     dominant_emotion = response['dominant_emotion']
 
-    # Return a formatted string with the sentiment label and score 
-    return "For the given statement, the system response is 'anger': {}, 'disgust': {}, 'fear': {}, 'joy': {} and 'sadness': {}. The dominant emotion is {}.".format(anger, disgust, fear, joy, sadness, dominant_emotion)
-   
+    # Check if the label is None, indicating an error or invalid input
+    if dominant_emotion is None:
+        return "Invalid input! Try again."
+    # Return a formatted string with the sentiment label and score
+    return (
+    f"For the given statement, the system response is "
+    f"'anger': {anger}, 'disgust': {disgust}, 'fear': {fear}, "
+    f"'joy': {joy} and 'sadness': {sadness}. "
+    f"The dominant emotion is {dominant_emotion}."
+)
 
 @app.route("/")
 def render_index_page():
@@ -34,9 +43,6 @@ def render_index_page():
         page over the Flask channel
     '''
     return render_template('index.html')
-    
 
 if __name__ == "__main__":
-    ''' This functions executes the flask app and deploys it on localhost:8000
-    '''
     app.run(host="0.0.0.0", port=5000)
